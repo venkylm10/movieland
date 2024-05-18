639fce320f3b4f1cdf217fbd536596ad235699c0
venylm10 <venkylm10@gmail.com>
2024-05-19 02:58:16 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState, useEffect} from "react";
 
-function App() {
+// const Person = (props) => {
+//   return (
+//     <div>
+//       <h1>First Name: {props.firstName}</h1>
+//       <h2>Last Name: {props.lastName}</h2>
+//       <h2>Age: {props.age}</h2>
+//     </div>
+//   );
+// };
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  useEffect(()=>{
+   alert('You have changed the counter to ' + count);
+  },[count]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> setCount((prevCount)=> prevCount - 1)}>-</button>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount((prevCount)=>prevCount+1)}>+</button>
     </div>
   );
-}
+};
 
 export default App;
